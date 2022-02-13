10e28849fd79be697fa91ecf5750fa4a7da3ec47
Nikhil Karnwal <nikhilkarnwal@yahoo.com>
2022-02-12 17:55:02 -0800

    update

--- scripts/train_irl/train_gail.sh
+++ scripts/train_irl/train_gail.sh
@@ -27,13 +27,13 @@ model_list=$(python -c "import mani_skill, os, os.path as osp; print(osp.abspath
 # train using states only
 
 # replay_buff="./full_mani_skill_state_data/OpenCabinetDrawer_state/custom_1000_link0_v0_v1.h5"
-# , \"./full_mani_skill_state_data/OpenCabinetDrawer_state/OpenCabinetDrawer_1000_link_1-v0.h5"
+replay_buff="./full_mani_skill_state_data/OpenCabinetDrawer_state/OpenCabinetDrawer_1000_link_1-v0.h5"
 c_t=$(date "+%d_%m_%Y_%H_%M_%S")
 model_dir="/media/biswas/D/maniskill/IRLStateSB/gail/sac/"
 base_dir="/media/biswas/D/maniskill"
 work_dir="work_dirs/irl_drawer/irl/v0/gail_sac_env1000"
 
-replay_buff="$base_dir/rl/v2/sac/env1000_link_0/finite_horizon_data/trajectory.h5"
+#replay_buff="$base_dir/rl/v2/sac/env1000_link_0/finite_horizon_data/trajectory.h5"
 mkdir -p $base_dir/$work_dir/$c_t/
 echo $1 > $base_dir/$work_dir/$c_t/desc.txt
 # #train airl
@@ -49,7 +49,7 @@ python -m tools.run_rl configs/v2/irl_s3_state.py --gpu-ids=0 \
 	--work-dir=$base_dir/$work_dir/$c_t/ \
 	--cfg-options "train_mfrl_cfg.total_steps=1" \
 	"env_cfg.env_name=OpenCabinetDrawer_1000_link_0-v0" "train_mfrl_cfg.init_replay_buffers=[$replay_buff]" \
-	"agent.policy_cfg.gail_cfg.algo=gail" \
+	"agent.policy_cfg.gail_cfg.algo=gail"  >  $base_dir/$work_dir/$c_t/temp_log.txt 
 	# "agent.policy_cfg.gail_cfg.resume_model=$model_dir/24_12_2021_17_25_56/sac_policy.zip" \
 	# "agent.policy_cfg.gail_cfg.reward_model=$model_dir/24_12_2021_17_25_56/reward" \
 	# "agent.policy_cfg.gail_cfg.resume=1" 
